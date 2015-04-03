3d302d10dd22e7c99aa154009bf848317fc28185
Chris Peters <chris@minimalorange.com>
2015-04-03 15:49:38 -0400

    Fixes #9 - Sinatra extension should not use Sinatra base namespace

--- lib/draftsman/frameworks/sinatra.rb
+++ lib/draftsman/frameworks/sinatra.rb
@@ -1,9 +1,11 @@
-module Sinatra
-  module Draftsman
+require 'active_support/core_ext/object' # provides the `try` method
+
+module Draftsman
+  module Sinatra
 
     # Register this module inside your Sinatra application to gain access to controller-level methods used by Draftsman
     def self.registered(app)
-      app.helpers Sinatra::Draftsman
+      app.helpers self
       app.before { set_draftsman_whodunnit }
     end
 
@@ -15,17 +17,20 @@ module Sinatra
     # Override this method in your controller to call a different
     # method, e.g. `current_person`, or anything you like.
     def user_for_draftsman
-      current_user if defined?(current_user)
+      return unless defined?(current_user)
+      ActiveSupport::VERSION::MAJOR >= 4 ? current_user.try!(:id) : current_user.try(:id)
+    rescue NoMethodError
+      current_user
     end
 
   private
 
     # Tells Draftsman who is responsible for any changes that occur.
     def set_draftsman_whodunnit
-      ::Draftsman.whodunnit = user_for_draftsman
+      ::Draftsman.whodunnit = user_for_draftsman if ::Draftsman.enabled?
     end
 
   end
 
-  register Sinatra::Draftsman if defined?(register)
+  ::Sinatra.register Draftsman::Sinatra if defined?(::Sinatra)
 end
