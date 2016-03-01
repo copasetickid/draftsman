1b5cbda7cdf7d1d074bc9233bb290e04ae884a5b
Chris Davis <defbyte@gmail.com>
2016-03-01 12:57:05 -0500

    More robust version module.

--- lib/draftsman/version.rb
+++ lib/draftsman/version.rb
@@ -1,3 +1,18 @@
 module Draftsman
-  VERSION = '0.3.7'
+  module VERSION
+    MAJOR = 0
+    MINOR = 3
+    TINY  = 7
+    PRE   = nil
+
+    STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
+
+    def self.to_s
+      STRING
+    end
+  end
+
+  def self.version
+    VERSION::STRING
+  end
 end
